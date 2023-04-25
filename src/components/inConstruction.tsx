504b385edf3e0e9a110f71d4b9e7c3da508e838a
dav033 <87620700+dav033@users.noreply.github.com>
2023-04-25 13:49:21 -0600

    [feat]: add a styled cooming son page

---
+++ src/components/inConstruction.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/inConstruction.scss";
+import image from "../assets/images/inWorking.jpg";
+import image2 from "../assets/images/inWorkingBack.jpg";
+import { BsInstagram } from "react-icons/bs";
+
+export default function InConstruction() {
+  return (
+    <div className="inConstruction">
+      <div className="imageContent">
+        <img src={image} style={{ margin: 0, padding: 0, display: "block" }} />
+      </div>
+
+      <div className="uwu" />
+
+      <div className="informationContent">
+        <h1>Cooming soon</h1>
+        <h5>We are working on our website</h5>
+        <a href="https://www.instagram.com/maros_construction/" target="_blank">
+          <BsInstagram className="instagramIcon" />
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-
-
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import InConstruction from "../components/inConstruction";
+import "../styles/global.css";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main >
-     This site is under development
+    <main
+      style={{
+        margin: 0,
+        height: "100%",
+      }}
+    >
+      <InConstruction />
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
